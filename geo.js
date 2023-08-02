79c564b6f2340a8bc39493c088d785614462b67f
danielysdev <beltrandanielys@gmail.com>
2023-08-01 22:49:47 -0400

    estilos

--- geo.js
+++ geo.js
@@ -20,7 +20,9 @@ const searchIP = (event) => {
     }
     
     //para agregar el valor del imput se agrega el ${inputIp.value},se quito el calor que tenia el link de la pag web que era 8.8.8.8 y cambia la comilla por la que parece el acento invertido
-    geoApi(`https://geo.ipify.org/api/v2/country,city?apiKey=at_7YNnrt5gTvVQniS4ncpyp4h3tO9zb&ipAddress=${inputIP.value}`)
+    https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_9jNbggiMVS8t2uUdShPbMNgRWarMb&ipAddress=8.8.8.8
+
+    geoApi(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_9jNbggiMVS8t2uUdShPbMNgRWarMb&ipAddress=${inputIP.value}`)
     .then((res) => {
         //con esto creo los valores sean dinamicos
         ip.innerText = res.ip;
